ed27cf92fe1e0866e42fe2bd044005fa2cb7bff4
Yuuitsurou Mine <yuuitsurou.mine@gmail.com>
2018-04-11 21:52:53 +0900

    helm

--- emacs/init.d/24/init/init-helm.el
+++ emacs/init.d/24/init/init-helm.el
@@ -12,9 +12,9 @@
 (setq helm-ff-auto-update-initial-value nil)
 ;; TAB で補完する
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
-(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
-(define-key helm-map (kbd "C-z") 'helm-select-action)
-;;(define-key helm-read-file-map (kbd "<tab>") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-]") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-:") 'helm-select-action)
+;(define-key helm-read-file-map (kbd "<tab>") 'helm-execute-persistent-action)
 ;; key setteings
 ;(global-set-key (kbd "C-c h") 'helm-mini)
 (global-set-key (kbd "M-x") 'helm-M-x)
@@ -22,7 +22,9 @@
 (global-set-key (kbd "C-x C-;") 'helm-for-files)
 (global-set-key (kbd "C-M-o") 'helm-occur)
 ;; プレフィックスキー C-;
-(custom-set-variables '(helm-command-prefix-key "C-;"))
+;;(custom-set-variables '(helm-command-prefix-key "C-;"))
+(global-set-key (kbd "C-;") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
 ;; キーバインドを設定する。C-; でコマンド起動後は、以下のキーが利用可能となる
 ;;  M-n     ：カーソル位置の単語を検索パターンに追加
 ;;  C-z     ：チラ見
@@ -35,6 +37,17 @@
 (define-key helm-command-map (kbd "b")   'helm-buffers-list)
 (define-key helm-command-map (kbd "i")   'helm-imenu)
 (define-key helm-command-map (kbd "C-i") 'helm-semantic-or-imenu)
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p t
+      ; helm-move-to-line-cycle-in-source t
+      helm-ff-search-library-in-sexp t
+      helm-scroll-amount 8
+      helm-ff-file-name-history-use-recentf t
+      helm-echo-input-in-header-line t)
+
 ;; Emulate `kill-line' in helm minibuffer
 (setq helm-delete-minibuffer-contents-from-point t)
 (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)

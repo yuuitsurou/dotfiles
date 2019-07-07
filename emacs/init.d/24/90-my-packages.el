1eee1bcfe57061067837ce9122784af5e007f8eb
Yuuitsurou Mine <ymine@dc4.so-net.ne.jp>
2019-07-07 12:42:09 +0900

    migemo

--- emacs/init.d/24/90-my-packages.el
+++ emacs/init.d/24/90-my-packages.el
@@ -87,14 +87,14 @@
   :config
   (setq migemo-command "cmigemo")
   (setq migemo-options '("-q" "--emacs"))
-  (setq migemo-dictionary "/usr/share/migemo/utf-8/migemo-dict")
+  (setq migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict")
   (setq migemo-user-dictionary nil)
   (setq migemo-regex-dictionary nil)
   (setq migemo-use-pattern-alist t)
   (setq migemo-use-frequent-pattern-alist t)
   (setq migemo-pattern-alist-length 1000)
   (setq migemo-coding-system 'utf-8-unix)
-  (load-library "migemo")
+  ;;(load-library "migemo")
   (migemo-init)
 )
 

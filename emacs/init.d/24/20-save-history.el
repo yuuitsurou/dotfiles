3ca0146a3af40c0ed4e415584e13f805e57dca36
Yuuitsurou Mine <yuuitsurou.mine@gmail.com>
2022-06-08 19:30:53 +0900

    20-save-history.el

---
+++ emacs/init.d/24/20-save-history.el
@@ -0,0 +1,11 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 履歴の保存
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(savehist-mode 1)
+(setq history-length t)
+(setq history-delete-duplicates t)
+(setq savehist-save-minibuffer-history 1)
+(setq savehist-additional-variables
+      '(kill-ring
+        search-ring
+        regexp-search-ring))
